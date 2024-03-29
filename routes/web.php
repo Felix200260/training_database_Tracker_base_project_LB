5ff98df5ee6e17727721b54999bf65e62a715dda
Felix <92216380+Felix200260@users.noreply.github.com>
2024-03-29 19:33:55 +0500

    ВЫПОЛНИЛ РАБОТУ: Защита маршрутов от доступа неаутентифицированных пользователей. переписал полностью маршруты и добавил по заданию. ПРичесал всё

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\GoalController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 
-// Route::resource('/goals', GoalController::class, 'index');
-// Route::resource('/users', UserController::class, 'index');
-
+// Обработка маршрутов для целей и пользователей
 Route::get('/goals', [GoalController::class, 'index']);
 Route::get('/user/{id}', [UserController::class, 'show']);
-//обрабатывает GET-запросы и используется для отображения списка всех пользователей.
 Route::get('/users', [UserController::class, 'index']);
 
+// Лабораторная работа: Разработка методов CRUD
+// Маршруты для работы с пользователями, доступны только аутентифицированным пользователям
+Route::middleware('auth')->group(function () {
+    // Отображение формы для создания нового пользователя
+    Route::get('/users/create', [UserController::class, 'create']);
+    // Обработка отправленной формы и сохранение нового пользователя
+    Route::post('/users', [UserController::class, 'store']);
+    // Отображение формы для редактирования пользователя
+    Route::get('/users/edit/{id}', [UserController::class, 'edit']);
+    // Обработка запроса на обновление данных пользователя
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    // Удаление пользователя
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
 
-//-----------------------Лабараторная работа Разработка методов CRUD---------------------------
-// Отображение формы для создания нового пользователя
-Route::get('/users/create', [UserController::class, 'create']);
-
-// Обработка отправленной формы и сохранение нового пользователя/ обрабатывает POST-запросы и используется для приёма данных из формы и создания нового пользователя в базе данных.
-Route::post('/users', [UserController::class, 'store']);
-
-Route::get('/users/edit/{id}', [UserController::class, 'edit']);
-
-Route::put('/users/{id}', [UserController::class, 'update']);
-
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-// Route::delete('/users', [UserController::class, 'destroy']);
-//-----------------------Лабараторная работа Разработка методов CRUD---------------------------
-//-----------------------Лабараторная работа Аутентификация пользователя с использованием метода Auth::attempt---------------------------
-Route::get('/login', [LoginController::class, 'login']);
-
+// Лабораторная работа: Аутентификация пользователя с использованием метода Auth::attempt
+// Маршруты для аутентификации
+Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::get('/logout', [LoginController::class, 'logout']);
-
 Route::post('/auth', [LoginController::class, 'authenticate']);
 
-//-----------------------Лабараторная работа Аутентификация пользователя с использованием метода Auth::attempt---------------------------
+// Лабораторная работа: Защита маршрутов от доступа неаутентифицированных пользователей
+// Маршрут /login теперь имеет имя 'login', используемое в middleware 'auth'
+// Это имя используется для перенаправления неаутентифицированных пользователей на страницу входа
 
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Базовый маршрут для страницы приветствия
 Route::get('/', function () {
     return view('welcome');
 });
